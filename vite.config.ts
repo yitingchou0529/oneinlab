af516415980348f2255729dfc6c163163c93fee3
yitingchou0529 <sfntsh0974026835@gmail.com>
2026-09-07 17:29:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/oneinlab/'
+    base:'/oneinlab/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
